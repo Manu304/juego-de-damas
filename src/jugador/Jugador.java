05653328f2c0cf1a88a9be106a976b444ad975c3
Manuel Rojas <manuerjs70@gmail.com>
2021-06-17 20:58:14 -0600

    Agregue getter y setter de jugador

--- src/jugador/Jugador.java
+++ src/jugador/Jugador.java
@@ -1,5 +1,63 @@
 package src.jugador;
 
 public class Jugador {
+
+    private String nombre;
+    private int perdidas, ganadas, puntuacion;
+
+    public Jugador(){
+        this("Random");
+    }
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPerdidas() {
+        return perdidas;
+    }
+
+    public int getGanadas() {
+        return ganadas;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(boolean gano, int puntos){
+        if (gano) {
+            puntuacion += puntos;
+        } else {
+            if (puntuacion - puntos > 0) {
+                puntuacion -= puntos;
+            } else {
+                puntuacion = 0;
+            }
+        }
+    }
+
+    public void setPartidas(boolean gano){
+        if (gano) {
+            ganadas++;
+        }else{
+            perdidas--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+    
     
 }
