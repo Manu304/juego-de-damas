bd0036b038e1173fff6a326588bdbebc6ec5f429
Manuel Rojas <manuerjs70@gmail.com>
2021-06-17 20:38:46 -0600

    Agregue metodos estaticos para pedir datos y generar numeros aleatorios

--- src/juego/ManejoInfo.java
+++ src/juego/ManejoInfo.java
@@ -1,5 +1,37 @@
 package src.juego;
 
+import java.util.Scanner;
+
 public class ManejoInfo {
-    
+    static Scanner scanner = new Scanner(System.in);
+
+    private static void imprimirMensaje(String mensaje) {
+        System.out.print("Ingrese " + mensaje + ": ");
+    }
+
+    public static int getEntero(String mensaje) {
+        imprimirMensaje(mensaje);
+        int tmp = scanner.nextInt();
+        if (tmp < 0) {
+            do {
+                imprimirMensaje("\nERROR. No puede ingresar números negativos\n");
+                imprimirMensaje(mensaje);
+                tmp = scanner.nextInt();
+            } while (tmp < 0);
+        }
+
+        return tmp;
+    }
+
+    public static String getTexto(String mensaje) {
+        imprimirMensaje(mensaje);
+        scanner.nextLine();
+        String res = scanner.nextLine().toLowerCase().trim();
+        return res;
+    }
+
+    public static int aleatorio(int minimo, int maximo) {
+        int aleatorio = (int) Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
+        return aleatorio;
+    }
 }
